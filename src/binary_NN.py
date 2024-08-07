fixes big problem with encoding, adds average roc and confusion matrics for CNN

src/binary_NN.py
@@ -1,17 +1,164 @@
-from database.utils import get_reconstructed_probabilities
-from models.SVM.binary_classification import get_best_estimators, get_scores_and_probs, optimise_score_over_channels
-from models.scoring_metrics import scoring_function, print_scores_for_channel
 from database.data import Data
-from sklearn.svm import SVC
-from tqdm import tqdm
+
+#used for cnn
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv1D, BatchNormalization, Activation, MaxPooling1D, Flatten, Dense, Dropout
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.callbacks import EarlyStopping
+
+#used to encoded strings to binary 
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
+#used for class weighting
+from sklearn.utils import class_weight
+
+import numpy as np
+
 
 
 train_splits = {}
 train_splits['train'] = 0.7
 train_splits['test'] = 0.3
 
-#creating DATA object
-ptb_binary_NN = Data(database = 'ptbdb', denoise_method='DWT', estimation_method = 'NN', train_splits=None, binary = True, parameterisation = False)
 
-ptb_binary_NN.run()
 
+def get_data_CNN(splits, denoise_method):
+    #train spit shouldnt be in here because takes ages to do first bits....
+
+    #creating DATA object
+    ptb_binary_NN = Data(database = 'ptbdb', denoise_method=denoise_method, estimation_method = 'NN', train_splits=splits, binary = True, parameterisation = False)
+
+    ptb_binary_NN.run()
+
+    if splits is None:
+        return ptb_binary_NN.input_data, ptb_binary_NN.labels
+        
+    else:
+        return ptb_binary_NN.split_data
+
+def time_shift(data, shift_max=50):
+    shift = np.random.randint(-shift_max, shift_max)
+    augmented_data = np.roll(data, shift)
+    return augmented_data
+
+def scale_data(data, scale_factor=0.1):
+    scale = np.random.uniform(1 - scale_factor, 1 + scale_factor)
+    augmented_data = data * scale
+    return augmented_data
+
+def augment_data(data):
+    augmented_data = []
+    for signal in data:
+        augmented_data.append(signal)
+        augmented_data.append(time_shift(signal))
+        augmented_data.append(scale_data(signal))
+    return np.array(augmented_data)
+
+def convert_data_to_CNN_format(data, channel_indices):
+    X_train_all = []
+    X_test_all = []
+    y_train_all = []
+    y_test_all = []
+
+    if channel_indices == 'all':
+        channel_indices = [0, 1, 2, 3, 4, 5]
+
+    #accessing all data in desired channels
+    for i in channel_indices:
+        X_train_all.append(data[i]['X_train'])
+        y_train_all.append(data[i]['y_train'])
+
+        X_test_all.append(data[i]['X_test'])
+        y_test_all.append(data[i]['y_test'])
+
+    X_train_all_list = [element for array in X_train_all for element in array]
+    y_train_all_list = [element for array in y_train_all for element in array]
+    X_test_all_list = [element for array in X_test_all for element in array]
+    y_test_all_list = [element for array in y_test_all for element in array]
+
+    #encoding y labels for CNN
+    # label_encoder = LabelEncoder()
+    # y_train_all_encoded = label_encoder.fit_transform(y_train_all_list)
+    # y_test_all_encoded = label_encoder.transform(y_test_all_list)
+    y_train_all_encoded = [0 if label == 'Unhealthy' else 1 for label in y_train_all_list]
+    y_test_all_encoded = [0 if label == 'Unhealthy' else 1 for label in y_test_all_list]
+
+    #changing into format appropriate for CNN
+    #CNN needs to know number of features per sample, i.e. 1D
+    X_train_all_cnn = np.expand_dims(X_train_all_list, axis=-1) 
+    y_train_all_cnn = np.expand_dims(y_train_all_encoded, axis=-1) 
+    X_test_all_cnn = np.expand_dims(X_test_all_list, axis=-1) 
+    y_test_all_cnn = np.expand_dims(y_test_all_encoded, axis=-1) 
+
+    #creating new dictionary
+    converted_data = {}
+    converted_data['X_train'] = X_train_all_cnn
+    converted_data['y_train'] = y_train_all_cnn
+    converted_data['y_test'] = y_test_all_cnn
+    converted_data['X_test'] = X_test_all_cnn
+
+    #finding class weight due to inbalanced class system
+    class_weights = class_weight.compute_class_weight('balanced', classes=np.unique(y_train_all_encoded), y=y_train_all_encoded)
+    class_weights_dict = {i: class_weights[i] for i in range(len(class_weights))}
+
+    return converted_data, y_test_all_list, class_weights_dict
+
+
+def make_model(depth, filters, k, print_summary = True):
+    # initialise model
+    cnn = Sequential()
+
+    for _ in range(depth):
+        cnn.add(Conv1D(filters=filters, kernel_size=k, padding='same', input_shape = (60000, 1)))
+        cnn.add(BatchNormalization())
+        cnn.add(Activation('relu'))
+        cnn.add(MaxPooling1D(pool_size=2))  # takes max out of every two
+        cnn.add(Dropout(0.5))
+
+    cnn.add(Flatten())
+    cnn.add(Dense(1, activation='sigmoid'))  # use 'softmax' for multi-class classification, 'sigmoid' for binary classification
+
+    # Compile the model
+    cnn.compile(optimizer=Adam(), loss='binary_crossentropy', metrics=['accuracy']) #binary cross entropy for binary classification
+
+    if print_summary:
+        print(cnn.summary())
+        
+    return cnn
+
+
+def train_model(model, data, class_weights, EPOCHS, BATCH_SIZE):
+    early_stopping = EarlyStopping(monitor='val_accuracy', patience=5, restore_best_weights=True)
+
+    history = model.fit(
+        data['X_train'], data['y_train'],
+        epochs=EPOCHS,
+        batch_size=BATCH_SIZE,
+        validation_data=(data['X_test'], data['y_test']),
+        class_weight=class_weights,
+        callbacks=[early_stopping]
+    )
+    return history, model
+
+
+def get_split_data(split_percents, data, labels):
+        split_data_dict = {}
+        train_split = split_percents['train']
+        test_split = split_percents['test']
+       # val_split = split_percents['val']
+        if train_split + test_split != 1:
+            print('percentages dont total 1')
+            return None
+        else:
+            print('splitting data into test and train')
+            
+            for i in range(0, 6):
+                split_data = {}
+                X_train, X_test, y_train, y_test = train_test_split(data[i], labels[i], test_size=test_split, stratify=labels[i])
+                split_data['X_train'] = X_train
+                split_data['X_test'] = X_test
+                split_data['y_train'] = y_train
+                split_data['y_test'] = y_test
+                split_data_dict[i] = split_data
+
+            return split_data_dict

src/models/SVM/binary_classification.py
@@ -107,9 +107,9 @@ def average_probabilities(probs, channels):
 def manual_y_predict(average_probs, threshold):#change this back to threshold and see if better??
     manual_y_pred = np.empty(len(average_probs), dtype=object)
     for j in range(0, len(average_probs)):
-        if average_probs[j] > 0.3:
+        if average_probs[j] > threshold:
             manual_y_pred[j] = 'Healthy'
-        elif average_probs[j] <= 0.3:
+        elif average_probs[j] <= threshold:
             manual_y_pred[j] = 'Unhealthy'
         elif average_probs[j] is None:
             manual_y_pred[j] = np.nan

src/models/scoring_metrics.py
@@ -29,6 +29,7 @@ def get_balanced_accuracy(y_test, y_pred):
     """
     num_healthy_true = np.sum([x=='Healthy' for x in y_test])
     num_unhealthy_true = len(y_test) - num_healthy_true
+
     count_healthy_accurate = 0
     count_unhealthy_accurate = 0
     for i in range(0, len(y_test)):
@@ -36,6 +37,7 @@ def get_balanced_accuracy(y_test, y_pred):
             count_unhealthy_accurate +=1
         elif y_pred[i] == y_test[i] == 'Healthy':
             count_healthy_accurate +=1
+
     healthy_percentage = count_healthy_accurate/num_healthy_true
     unhealthy_percentage = count_unhealthy_accurate/num_unhealthy_true
     balanced_accuracy = (healthy_percentage + unhealthy_percentage) * 0.5
@@ -48,9 +50,9 @@ def get_specificity(y_test, y_pred):
     true_negative = 0
     false_positive = 0
     for i in range(0, len(y_test)):
-        if y_pred[i] == y_test[i] == 'Healthy':
+        if y_pred[i] == y_test[i] == 'Unhealthy':
             true_negative += 1
-        elif y_pred[i] != y_test[i] and y_test[i] == 'Healthy':
+        elif y_pred[i] != y_test[i] and y_test[i] == 'Unhealthy':
             false_positive += 1
     
     return true_negative / (true_negative+false_positive)
@@ -59,9 +61,9 @@ def get_precision(y_test, y_pred):
     true_positive = 0
     false_positive = 0
     for i in range(0, len(y_test)):
-        if y_pred[i] == y_test[i] == 'Unhealthy':
+        if y_pred[i] == y_test[i] == 'Healthy':
             true_positive += 1
-        elif y_pred[i] != y_test[i] and y_test[i] == 'Healthy':
+        elif y_pred[i] != y_test[i] and y_test[i] == 'Unhealthy':
             false_positive += 1
     return true_positive/(true_positive + false_positive)
 
@@ -69,9 +71,9 @@ def get_recall(y_test, y_pred):
     false_negative = 0
     true_positive = 0
     for i in range(0, len(y_test)):
-        if y_pred[i] == y_test[i] == 'Unhealthy':
+        if y_pred[i] == y_test[i] == 'Healthy':
             true_positive += 1
-        elif y_pred[i] != y_test[i] and y_test[i] == 'Unhealthy':
+        elif y_pred[i] != y_test[i] and y_test[i] == 'Healthy':
             false_negative += 1
     return true_positive/(true_positive + false_negative)
 
@@ -84,11 +86,11 @@ def get_f1_score(y_test, y_pred):
     false_positive = 0
     false_negative = 0
     for i in range(0, len(y_test)):
-        if y_pred[i] == y_test[i] == 'Unhealthy':
+        if y_pred[i] == y_test[i] == 'Healthy':
             true_positive += 1
-        elif y_pred[i] != y_test[i] and y_test[i] == 'Healthy':
-            false_positive += 1
         elif y_pred[i] != y_test[i] and y_test[i] == 'Unhealthy':
+            false_positive += 1
+        elif y_pred[i] != y_test[i] and y_test[i] == 'Healthy':
             false_negative += 1
     return (2*true_positive)/(2*true_positive+false_positive+false_negative)
 
@@ -150,20 +152,14 @@ def print_scores_for_channel(all_scores):
 
     return None
 
-    # #presenting results as pandas df
-    # data = {
-    #     'Success Metric': ['Objective Score', 'Balanced Accuracy'],
-    #     'Channel 1': [f'{scores[0]['objective score']}', f'{scores[0]['balanced_accuracy']}],
-    #     'Channel 2': [f'{score_accuracy[1]}', f'{balanced_accuracy[1]}'],
-    #     'Channel 3': [f'{score_accuracy[2]}', f'{balanced_accuracy[2]}'],
-    #     'Channel 4': [f'{score_accuracy[3]}', f'{balanced_accuracy[3]}'],
-    #     'Channel 5': [f'{score_accuracy[4]}', f'{balanced_accuracy[4]}'],
-    #     'Channel 6': [f'{score_accuracy[5]}', f'{balanced_accuracy[5]}'],
-        
-    # }
 
-    # df = pd.DataFrame(data)
-    # print(df)
+def get_all_metrics(y_test, y_pred):
+    metrics = {}
+    metrics['bal acc'] = get_balanced_accuracy(y_test, y_pred)
+    metrics['accuracy'] = get_accuracy(y_test, y_pred)
+    metrics['f1'] = get_f1_score(y_test, y_pred)
+    metrics['obj score'] = objective_score(y_test, y_pred)
+    metrics['recall'] = get_recall(y_test, y_pred)
+    metrics['precision'] = get_precision(y_test, y_pred)
 
-    # return None
-
+    return metrics